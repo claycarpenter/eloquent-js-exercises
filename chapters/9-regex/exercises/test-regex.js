fda1d75d106795afe481d36ff7896d76f69e8f1d
Clay Carpenter <claycarpenter@gmail.com>
2015-03-12 23:12:21 +0000

    Adds support for reporting the results of a run of tests.

--- chapters/9-regex/exercises/test-regex.js
+++ chapters/9-regex/exercises/test-regex.js
@@ -4,13 +4,21 @@
     function testRegex(pattern, tests) {
         console.log('\nTesting regex pattern ' + pattern + ':');
     
+        var successCounter = 0;
+        
         tests.forEach(function(test) {
             var matches = pattern.test(test.string);
             
             var resultLabel = (matches == test.expected) ? colors.green('PASS') : colors.red('FAIL');
+            
+            if (resultLabel) successCounter++;
     
             console.log('  ' + resultLabel + ' :: \"' + test.string + '\", expected ' + test.expected);
         });
+        
+        console.log(
+            '\nTest results: ', successCounter, 'of', tests.length, 
+            (tests.length > 0 ? 'tests' : 'test'), 'passed.\n');
     }
     
     function Test(string, expected) {

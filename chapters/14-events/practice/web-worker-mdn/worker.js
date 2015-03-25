72c3e4e60e9ec4294199d71b2ef5937dffdf1aec
Clay Carpenter <claycarpenter@gmail.com>
2015-03-25 19:40:42 -0400

    Adds web workers example.

---
+++ chapters/14-events/practice/web-worker-mdn/worker.js
@@ -0,0 +1,9 @@
+onmessage = function (e) {
+    console.log('Worker: Message received from main script.');
+    
+    var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
+    
+    console.log('Worker: Sending result message back to main script.');
+    
+    postMessage(workerResult);
+};

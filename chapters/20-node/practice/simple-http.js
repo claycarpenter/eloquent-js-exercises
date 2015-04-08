54a022bf9272d9e9e40d33f528ea1b53bf954e13
Clay Carpenter <claycarpenter@gmail.com>
2015-04-08 23:47:59 +0000

    Adds a small log message to better illustrate how the callback given to createServer is executed for each request.

--- chapters/20-node/practice/simple-http.js
+++ chapters/20-node/practice/simple-http.js
@@ -2,6 +2,8 @@
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
+    console.log('Responding to', request.method, 'request for', request.url);
+    
     response.writeHead(200, {'Content-type': 'text/html'});
     
     response.write('<h1>Hello!</h1><p>You asked for <code>' + request.url + '</code></p>');

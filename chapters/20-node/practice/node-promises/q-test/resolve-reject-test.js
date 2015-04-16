7b03f36090b72c1c72925b7dc10cee7af9f94eb0
Clay Carpenter <claycarpenter@gmail.com>
2015-04-16 00:17:09 +0000

    Adds example of Q.deferred .resolve, .reject methods.

---
+++ chapters/20-node/practice/node-promises/q-test/resolve-reject-test.js
@@ -0,0 +1,38 @@
+
+var Q = require('q');
+
+function makeDeferred () {
+    var deferred = Q.defer();
+    
+    deferred.promise
+        .then(success)
+        .catch(fail)
+        .finally(fin);
+    
+    return deferred;
+}
+
+function success (result) {
+    console.log('Console:', result);
+    
+    return result;
+}
+
+function fail (error) {
+    console.error('Error:', error);
+    
+    // Should an error be returned here?
+    return error;
+}
+
+function fin (result) {
+    // Looks like result is not given a value.
+    console.log('Fin!', result);
+}
+
+var yay = makeDeferred(),
+    nay = makeDeferred();
+
+yay.resolve('OH HAI!');
+
+nay.reject('Oh dear God no!');
